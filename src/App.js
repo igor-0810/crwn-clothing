5d331d8b64c2880b4e7fc391537ab6dd3fb78c92
Your Name <igorveles2004@yahoo.com>
2020-02-10 13:50:05 +0100

    added ivan folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <HomePage />
     </div>
   );
 }
